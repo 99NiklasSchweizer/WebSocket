6b54c5f795f21c4afe1c40288611c4407a1522bf
Niklas Schweizer <dumblexx@gmail.com>
2018-10-16 13:52:34 +0200

    löst uppgiften

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,35 @@
 /* eslint-disable no-unused-vars */
 import style from '../css/style.css';
 /* eslint-enable no-unused-vars */
+import password from './config';
 
 const connection = new WebSocket('ws://104.248.143.87:1337');
+
+connection.onopen = () => {
+  console.log('Uppkopling startad ... ?');
+};
+connection.onerror = error => {
+  console.log(`Èrror: ${error}`);
+};
+connection.onmessage = message => {
+  console.log(message);
+  const textEl = document.getElementById('logger');
+  const obj = JSON.parse(message.data);
+  if (obj.type === 'heartbeat') {
+    textEl.textContent += `${obj.data}, `;
+  }
+};
+connection.onclose = () => {
+  console.log('Uppkoppling nedstängd ...');
+};
+
+const btnEl = document.getElementById('btn');
+btnEl.addEventListener('click', () => {
+  const obj = {
+    type: 'heartbeat',
+    data: '',
+    key: password
+  };
+  const jsonObj = JSON.stringify(obj);
+  connection.send(jsonObj);
+});
